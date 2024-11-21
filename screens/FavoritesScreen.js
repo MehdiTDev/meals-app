ff133ecd1cd96522b7f0e44c7b6e0cbca91816a0
Mehdi <meh.tavakoli@gmail.com>
2024-11-21 17:16:18 +0100

    favite meals stored with context.

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,15 +1,32 @@
 import { View, Text, StyleSheet } from "react-native";
+import MealsList from "../components/MealsList/MealsList";
+import { useContext } from "react";
+import { FavoritesContext } from "../store/context/favorites-context";
+import { MEALS } from "../data/dummy-data";
 
 function FavoritesScreen() {
+  const favoriteMealsCtx = useContext(FavoritesContext);
+
+  const favoriteMeals = MEALS.filter((meal) =>
+    favoriteMealsCtx.ids.includes(meal.id)
+  );
+
+  if (favoriteMeals.length === 0) {
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meals yet!</Text>
+      </View>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Favorites</Text>
+    <View style={styles.rootContainer}>
+      <MealsList items={favoriteMeals} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootContainer: {
     flex: 1,
     backgroundColor: "#3f2f25",
     alignItems: "center",
